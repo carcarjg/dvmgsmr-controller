5da8147aeb0ece233064de09b5f6ceb9a9ab7ea7
Nyx <hello@rosesam.pw>
2025-06-14 20:29:27 -0500

    Remove RC2... Dont feel like dealing with moving an election app to C#

--- dvmgsmrcontroller/Properties/Resources.Designer.cs
+++ dvmgsmrcontroller/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace dvmgsmrcontroller.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _48450756326_5aab7b8186_b {
+            get {
+                object obj = ResourceManager.GetObject("48450756326_5aab7b8186_b", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap AppIcon {
             get {
                 object obj = ResourceManager.GetObject("AppIcon", resourceCulture);
