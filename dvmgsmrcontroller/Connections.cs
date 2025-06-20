6360cd2460eab923b642503c97f082fc5a521d73
Nyx <hello@rosesam.pw>
2025-06-20 08:10:21 -0500

    Update Connections.cs

--- dvmgsmrcontroller/Connections.cs
+++ dvmgsmrcontroller/Connections.cs
@@ -1,13 +1,37 @@
-﻿using WebSocketSharp;
+﻿using SIPSorcery.Media;
+using SIPSorcery.Net;
+using WebSocketSharp;
+using Windows.Media.Playback;
 
 namespace dvmgsmrcontroller
 {
 	internal class Connections
 	{
+		internal int WEBSOCKET_PORT { get; set; }
+
+		internal string WEBSOCKET_URL { get; set; }
+
+		internal WebSocket WS { get; set; }
+
 		internal void Start()
-		{ }
+		{
+			if (WEBSOCKET_URL != null)
+			{
+				if (WEBSOCKET_PORT != 0)
+				{
+					WS = new WebSocket("ws://" + WEBSOCKET_URL + WEBSOCKET_PORT);
+
+					//ws://URL:PORT/rtc for RTC
+					WS.OnMessage += OnMessage;
+				}
+			}
+		}
 
 		internal void Stop()
 		{ }
+
+		internal void OnMessage(object? sender, MessageEventArgs e)
+		{
+		}
 	}
 }
