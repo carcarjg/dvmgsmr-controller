2023ba5aa5925ac0bd7c1ff4b15e225f9cf728d9
Nyx <hello@rosesam.pw>
2025-10-24 14:55:51 -0500

    Ooohhh ASCI Art

--- dvmgsmrcontrollertester/Program.cs
+++ dvmgsmrcontrollertester/Program.cs
@@ -1,21 +1,41 @@
-﻿using SerialPortLib;
+﻿// %%%%%%    @%%%%%@
+//%%%%%%%%   %%%%%%%@
+//@%%%%%%%@  %%%%%%%%%        @@      @@  @@@      @@@ @@@     @@@ @@@@@@@@@@   @@@@@@@@@
+//%%%%%%%%@ @%%%%%%%%       @@@@@   @@@@ @@@@@   @@@@ @@@@   @@@@ @@@@@@@@@@@@@@@@@@@@@@@ @@@@
+// @%%%%%%%%  %%%%%%%%%      @@@@@@  @@@@  @@@@  @@@@   @@@@@@@@@     @@@@    @@@@         @@@@
+//  %%%%%%%%%  %%%%%%%%@     @@@@@@@ @@@@   @@@@@@@@     @@@@@@       @@@@    @@@@@@@@@@@  @@@@
+//   %%%%%%%%@  %%%%%%%%%    @@@@@@@@@@@@     @@@@        @@@@@       @@@@    @@@@@@@@@@@  @@@@
+//    %%%%%%%%@ @%%%%%%%%    @@@@ @@@@@@@     @@@@      @@@@@@@@      @@@@    @@@@         @@@@
+//    @%%%%%%%%% @%%%%%%%%   @@@@   @@@@@     @@@@     @@@@@ @@@@@    @@@@    @@@@@@@@@@@@ @@@@@@@@@@
+//     @%%%%%%%%  %%%%%%%%@  @@@@    @@@@     @@@@    @@@@     @@@@   @@@@    @@@@@@@@@@@@ @@@@@@@@@@@
+//      %%%%%%%%@ @%%%%%%%%
+//      @%%%%%%%%  @%%%%%%%%
+//       %%%%%%%%   %%%%%%%@
+//         %%%%%      %%%%
+//
+// (C) Nyx Gallini 2025
+//
+using SerialPortLib;
 using System.IO.Ports;
 using System.Timers;
 using HeadComLib;
 
-namespace dvmgsmrcontrollertester 
+namespace dvmgsmrcontrollertester
 {
-	class Program 
+	internal class Program
 	{
 		private static System.Timers.Timer StatTimer;
+
 		private static SerialPort _serialPort;
+
 		private bool HeadhelthOk;
+
 		private bool runonce;
 
 		public static void Main(string[] args)
 		{
 			Program MP = new Program();
-			
+
 			StatTimer = new System.Timers.Timer();
 
 			_serialPort = new SerialPort();
@@ -23,14 +43,14 @@ namespace dvmgsmrcontrollertester
 			_serialPort.BaudRate = 115200;
 			_serialPort.Open();
 			MP.Setupstatustimer();
-			
+
 			while (true)
 			{
 				string a = _serialPort.ReadExisting();
-				if (a != null && a != ""&& a != " ") { MP.CmdRx(a.Substring(0, 6)); }
-				
+				if (a != null && a != "" && a != " ") { MP.CmdRx(a.Substring(0, 6)); }
+
 				Thread.Sleep(200);
-				if (MP.runonce == false) 
+				if (MP.runonce == false)
 				{
 					MP.runonce = true;
 					MP.CmdTx(CmdsOutbound.OOPHeadcode + "  77X7");
@@ -50,11 +70,11 @@ namespace dvmgsmrcontrollertester
 					Thread.Sleep(5000);
 					MP.CmdTx(CmdsOutbound.OOPdsdOff);
 				}
-			}			
+			}
 		}
-		private void Setupstatustimer() 
-		{
 
+		private void Setupstatustimer()
+		{
 			CmdTx(CmdsOutbound.OOPControllerReady);
 			StatTimer = new System.Timers.Timer(5000);
 			StatTimer.Elapsed += StatusCheck;
@@ -62,63 +82,82 @@ namespace dvmgsmrcontrollertester
 			StatTimer.Enabled = true;
 			Console.WriteLine("Setup Completed");
 		}
+
 		internal void CmdRx(string cmd)
 		{
 			switch (cmd)
 			{
 				case CmdsInbound.IOPnack:
 					break;
+
 				case CmdsInbound.IOPack:
 					HeadhelthOk = true;
 					break;
+
 				case CmdsInbound.IOPHeadReady:
 					CmdTx(CmdsOutbound.OOPControllerReady);
 					break;
+
 				case CmdsInbound.IOPb1:
 					Console.WriteLine("Button1");
 					break;
+
 				case CmdsInbound.IOPb2:
 					Console.WriteLine("Button2");
 					break;
+
 				case CmdsInbound.IOPb3:
 					Console.WriteLine("Button3");
 					break;
+
 				case CmdsInbound.IOPb4:
 					Console.WriteLine("Button4");
 					break;
+
 				case CmdsInbound.IOPb5:
 					Console.WriteLine("Button5");
 					break;
+
 				case CmdsInbound.IOPb6:
 					Console.WriteLine("Button6");
 					break;
+
 				case CmdsInbound.IOPb7:
 					Console.WriteLine("Button7");
 					break;
+
 				case CmdsInbound.IOPb8:
 					Console.WriteLine("Button8");
 					break;
+
 				case CmdsInbound.IOPb9:
 					Console.WriteLine("Button9");
 					break;
+
 				case CmdsInbound.IOPb10:
 					Console.WriteLine("Button10");
 					break;
+
 				case CmdsInbound.IOPb11:
 					Console.WriteLine("Button11");
 					break;
+
 				case CmdsInbound.IOPb12:
 					Console.WriteLine("Button12");
 					break;
+
 				case CmdsInbound.IOPb13:
 					Console.WriteLine("Button13");
 					break;
+
 				case CmdsInbound.IOPb14:
 					Console.WriteLine("Button14");
 					break;
+
 				case CmdsInbound.IOPb15:
 					Console.WriteLine("Button15");
 					break;
+
 				case CmdsInbound.IOPb16:
 					Console.WriteLine("Button16");
 					break;
@@ -139,6 +178,5 @@ namespace dvmgsmrcontrollertester
 			if (HeadhelthOk == false) { Console.WriteLine("***Lost Head***"); }
 			else { Console.WriteLine("Head Health Ok"); }
 		}
-
 	}
 }
